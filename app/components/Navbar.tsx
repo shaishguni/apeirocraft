fab07312b12a07e88908b781640e4a2fad8c3d23
shaishguni <gunisguni2007@gmail.com>
2024-10-05 07:33:14 +0545

    .

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,23 +1,34 @@
+'use client'
+
 import React from 'react'
 import logo from "../../public/logo.png"
 import Image from 'next/image'
 import Link from 'next/link'
+import { ModeToggle } from './ui/ThemeToggle'
+import { Button } from '@/components/ui/button'
 
 const Navbar = () => {
   return (
-    <div className=' rounded-full border-2 m-2   ml-64 flex w-[800px]'>
+    <div className=' rounded-full border-2 m-2 space-x-96 items-center ml-48 justify-between flex w-[1000px]'>
         <nav  className='p-2 m-2 space-x-6 space-y-2  w-[300px] pl-12 flex '>
             <section className='flex space-x-2 mr-32 space-y-2'>
             <Image src={logo} width={50} height={50} alt="gg"/>
-            <h1 className='font-extrabold'>Apeirocraft</h1>
+            <Link href='/' className='font-extrabold'>Apeirocraft</Link>
             </section>
-        <div className='flex  space-x-6 '>
+        <div className='flex  space-x-6   '>
         <Link href="/">Product</Link>
         <Link href="/">Pricing</Link>
         <Link href="/">FAQ</Link>
         <Link href="/">Contact</Link>
+        
         </div>
-    </nav></div>
+        
+    </nav>
+    <div className='flex-1 p-4 space-x-2 space-y-2 m-2 items-end pl-2 ' >
+      <ModeToggle/>
+      <Button>Make It {"Your's"}</Button>
+    </div>
+    </div>
 
     
   )
