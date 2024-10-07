3498b67c5b1c03a78107815859eb09b14919422f
shaishguni <gunisguni2007@gmail.com>
2024-10-07 12:33:06 +0545

    .

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div className='w-full h-[800px] '>
-      <span className="w-auto px-6 py-3 rounded-full bg-secondary">
+    <div className='space-x-12  bg-primary/10 h-[800px] '>
+      <span className="w-auto px-6 py-3 top-6 rounded-full bg-secondary">
             <span className="text-sm font-medium text-primary ">Sort your notes easily</span>
       </span>
     </div>
