95a89c8f812d5afc0dbbe37c992aa1690d2c38e4
shaishguni <gunisguni2007@gmail.com>
2024-10-08 08:17:20 +0545

    .

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
   return (
     <div className='space-x-12  bg-primary/10 h-[800px] '>
       <span className="w-auto px-6 py-3 top-6 rounded-full bg-secondary">
-            <span className="text-sm font-medium text-primary ">Sort your notes easily</span>
+            <span className="text-sm font-medium text-primary "><span className="text-sm  rounded-lg font-medium bg-primary text-primary ">Sort </span> your notes easily</span>
       </span>
     </div>
   )
