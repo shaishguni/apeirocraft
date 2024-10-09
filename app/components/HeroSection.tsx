616f43409762123d871b77fd57e3948e5ab66324
shaishguni <gunisguni2007@gmail.com>
2024-10-09 07:09:08 +0545

    .

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -3,14 +3,15 @@ import React from 'react'
 const HeroSection = () => {
   return (
     <div className='space-x-12 items-center justify-between space-y-12 flex mt-4  pl-[650px] pb-[350px]    h-[600px] '>
-      <div className='items-center justify-between'>
-      <span className="w-auto px-6 py-3 top-6  rounded-full border shadow-inner border-spacing-6 ">
+      <div className='items-center flex space-y-12 flex-col justify-between'>
+      <span className="w-auto px-6 py-3 top-6  rounded-full border border-primary/25 shadow-inner border-spacing-1 ">
             <span className="text-sm font-extrabold   "><span className="text-sm  rounded-full p-2 font-extrabold text-white bg-primary ">Enhance</span> Your Digital Presence!</span>
       </span>
-      </div>
       <div>
-        <h1></h1>
+        <h1 className='text-5xl font-extrabold'>Experience The </h1>
       </div>
+      </div>
+      
       
     </div>
   )
