3b03a2323a603d0c5a492c7db85359bcb9760bec
shaishguni <gunisguni2007@gmail.com>
2024-10-11 07:27:45 +0545

    .

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,14 +1,15 @@
 
 const HeroSection = () => {
   return (
-    <div className='space-x-12 items-center justify-between space-y-12 flex mt-4  pl-[650px] pb-[350px]    h-[600px] '>
-      <div className='items-center flex space-y-12 flex-col justify-between'>
+    <div className='flex items-center  justify-center  bg-blue-100  h-[600px]     '>
+      <div className='items-center flex space-y-6 flex-col justify-between'>
       <span className="w-auto px-6 py-3 top-6  rounded-full border border-primary/25 shadow-inner border-spacing-1 ">
             <span className="text-sm font-extrabold   "><span className="text-sm  rounded-full p-2 font-extrabold text-white bg-primary ">Enhance</span> Your Digital Presence!</span>
       </span>
       <div>
-        <h1 className='text-5xl font-extrabold'>Experience The </h1>
+      <h1 className="text-4xl">Embark on a Digital Odyssey with <br /> AperioCraft Creative Agency</h1>
       </div>
+     
       </div>
     </div>
   )
