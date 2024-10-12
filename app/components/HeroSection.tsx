21d0a0652684ea6aaac9a8711ccea08c03a8af6e
shaishguni <gunisguni2007@gmail.com>
2024-10-12 07:56:08 +0545

    .

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,14 +1,14 @@
 
 const HeroSection = () => {
   return (
-    <div className='flex items-center  justify-center  bg-blue-100  h-[600px]     '>
+    <div className='flex items-center   justify-between  bg-blue-100  p-00px]  h-fit     '>
       <div className='items-center flex space-y-6 flex-col justify-between'>
       <span className="w-auto px-6 py-3 top-6  rounded-full border border-primary/25 shadow-inner border-spacing-1 ">
             <span className="text-sm font-extrabold   "><span className="text-sm  rounded-full p-2 font-extrabold text-white bg-primary ">Enhance</span> Your Digital Presence!</span>
       </span>
-      <div>
-      <h1 className="text-4xl">Embark on a Digital Odyssey with <br /> AperioCraft Creative Agency</h1>
-      </div>
+
+      <h1 className="text-6xl font-bold ">Embark on a Digital Odyssey with <br /> AperioCraft Creative Agency</h1>
+
      
       </div>
     </div>
