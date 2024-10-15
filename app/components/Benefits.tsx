434387b00c0f39c9576dc36d5147fdb2376fd070
shaishguni <gunisguni2007@gmail.com>
2024-10-15 07:39:32 +0545

    .

---
+++ app/components/Benefits.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Benefits = () => {
+  return (
+    <div>Benifits</div>
+  )
+}
+
+export default Benefits
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import HeroSection from "./components/HeroSection";
-
+import Benefits from "./components/Benefits";
 export default function Home() {
   return (
     <div>
      <HeroSection/> 
+     <Benefits/>
     </div>
         
     
