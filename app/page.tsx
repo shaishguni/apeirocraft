b276cc844c4e642d89f4504e85dd0da4ee7c85ec
shaishguni <gunisguni2007@gmail.com>
2024-10-15 09:09:23 +0545

    benifit made

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import HeroSection from "./components/HeroSection";
 import Benefits from "./components/Benefits";
 export default function Home() {
   return (
-    <div>
+    <div className="flex-row w-full h-full bg-white px-auto py-auto">
      <HeroSection/> 
      <Benefits/>
     </div>
