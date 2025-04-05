915166ef1de8f71b260c026f5955c5df959d8a07
shaishguni <gunisguni2007@gmail.com>
2025-04-05 07:47:25 +0545

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,23 +5,17 @@ import { ThemeProvider } from "./components/ui/theme-provider";
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
-
-
-
-
-
-
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900", 
 });
-
 
 export const metadata: Metadata = {
   title: "ApeiroCraft",
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use server";
+
 import { HeroSection } from "./components/HeroSection";
 import Benefits from "./components/Benefits";
 import { Features } from "./components/Features";
@@ -8,11 +10,20 @@ import { Contact } from "./components/Contact";
 import FAQ from "./components/FAQ";
 import Navbar from "./components/Navbar";
 
+import { redirect } from 'next/navigation';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 
 
 
-export default function Home() {
+export default async function  Home() {
+  const { isAuthenticated } = getKindeServerSession();
+  const authenticated =  await isAuthenticated();
+        
+  if (authenticated) {
+    redirect('/dashboard');
+  } 
   return (
+
     <div className="flex-row w-full overflow-none  space-y-12 h-full items-center justify-between md:p-12 mx-0 my-0 ">
       <Navbar/>
      <HeroSection/> 
